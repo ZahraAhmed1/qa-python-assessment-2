d2b8e709fec8e566ac3b2762d5bcde6e51e336bc
ZahraAhmed1 <zahra.1998@hotmail.co.uk>
2021-06-18 09:42:11 +0000

    question 2/3

--- questions/__init__.py
+++ questions/__init__.py
@@ -1,14 +1,20 @@
-def two(number):
-    prime = True 
-    if number < 1:
-        prime = False
-    if number > 1:
-        for i in range (2, number):
-            if number % i == 0:
-                prime = False
+# def two(number):
+#     prime = True 
+#     if number < 1:
+#         prime = False
+#     if number > 1:
+#         for i in range (2, number):
+#             if number % i == 0:
+#                 prime = False
 
-    return prime
+#     return prime
 
-print( two(3))
-print (two(16))
-print( two(19))
+# print( two(3))
+# print (two(16))
+# print( two(19))
+
+def three(a):
+    return (a*1111) + (a*111) + (a*11) + (a)
+
+print (three(5))
+print (three(9))
